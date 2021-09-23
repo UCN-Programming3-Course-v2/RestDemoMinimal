0a6dd4fb413d16cd1e3501c16f1f9760453fcf8b
Lars Nysom <LANY@ucn.dk>
2021-09-23 08:16:42 +0200

    Add project files.

---
+++ RestGreetingDemo/Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestGreetingDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase {
+
+        [HttpGet]
+        public string Get() {
+            return "Hello world";
+        }
+
+        [HttpGet, Route("{name}")]
+        public string Get(string name) {
+            string helloString = "Hello " + name;
+            return helloString;
+        }
+
+        [HttpGet, Route("{name}/{times}")]
+        public string Get(string name, int times) {
+            string helloString = "Hi " + name + " " + times + " times";
+            return helloString;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value) {
+        }
+
+    }
+}
